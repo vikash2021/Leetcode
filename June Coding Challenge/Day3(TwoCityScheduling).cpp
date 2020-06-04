51b17590b01c17caf7c675cd4ef15e331b8113c5
Vikash Kumar <kumarv4624@gmail.com>
2020-06-04 07:21:49 +0530

    Create Day3(TwoCityScheduling).cpp

---
+++ June Coding Challenge/Day3(TwoCityScheduling).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static bool compare(vector<int>&a, vector<int>& b)
+    {
+        return a[1]-a[0]>b[1]-b[0];
+    }
+    
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        
+        int n=costs.size();
+        sort(costs.begin(),costs.end(),compare);
+        int ans=0;
+            for(int i=0;i<n;++i)
+                ans+=i>=n/2?costs[i][1]:costs[i][0];
+        
+        return ans;
+        
+    }
+};
