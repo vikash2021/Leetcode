255884efb663ee3cea3b75c71abcc0df2114e69b
Vikash Kumar <kumarv4624@gmail.com>
2020-06-09 20:24:52 +0530

    Create Day9(IsSubsequnce).cpp

---
+++ June Coding Challenge/Day9(IsSubsequnce).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int m=s.length();
+        int n=t.length();
+        
+        int j = 0;
+
+        for (int i=0; i<n&&j<m; i++) 
+            if (s[j] == t[i]) 
+             j++; 
+  
+  
+        return (j==m); 
+
+        
+    }
+};
